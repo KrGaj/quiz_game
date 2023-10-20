3bbfc08e2ea026980aeb46ed4acc3c1a68a00b10
Krystian Gaj <krystian0120@outlook.com>
2023-10-20 17:04:50 +0200

    Fix displaying categories

--- app/src/main/java/com/example/codingquiz/ui/screen/CategoriesScreen.kt
+++ app/src/main/java/com/example/codingquiz/ui/screen/CategoriesScreen.kt
@@ -1,29 +1,30 @@
 package com.example.codingquiz.ui.screen
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.material3.Card
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.MaterialTheme.typography
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.codingquiz.data.domain.Category
 import com.example.codingquiz.ui.theme.CodingQuizTheme
 import com.example.codingquiz.viewmodel.CategoryViewModel
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
-fun Categories() {
-    // TODO
-}
-
-@Preview(showBackground = true)
-@Composable
 fun CategoryGrid(
-    categoryViewModel: CategoryViewModel = koinViewModel()
+    categoryViewModel: CategoryViewModel = koinViewModel(),
+    onItemClicked: (Category) -> Unit,
 ) {
     val categories = categoryViewModel.categories.collectAsState()
 
@@ -32,10 +33,46 @@ fun CategoryGrid(
             columns = GridCells.Fixed(count = CategoryViewModel.COLUMNS_NUM),
         ) {
             items(categories.value) { item ->
-                Card(modifier = Modifier.padding(16.dp)) {
-                    Text(text = item.name)
+                Category(
+                    name = item.name,
+                ) {
+                    onItemClicked(item)
                 }
             }
         }
     }
 }
+
+@Composable
+fun Category(
+    name: String,
+    onClick: () -> Unit = {},
+) {
+    CodingQuizTheme {
+        FilledTonalButton(
+            modifier = Modifier.padding(16.dp).fillMaxWidth(),
+            onClick = onClick,
+        ) {
+            Text(
+                text = name,
+                modifier = Modifier.padding(8.dp).fillMaxWidth(),
+                textAlign = TextAlign.Center,
+                style = typography.headlineMedium,
+            )
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewCategory() {
+    Box(modifier = Modifier.width(200.dp)) {
+        Category(name = "Demo")
+    }
+}
+
+//@Preview
+//@Composable
+//fun PreviewCategoryGrid() {
+//
+//}
