937c2248a531009d0f6a5428a9d931b519ec447e
Krystian Gaj <krystian0120@outlook.com>
2024-01-15 18:10:17 +0100

    Use icons in quiz summary screen to indicate whether answer was correct

--- app/src/main/java/com/example/codingquiz/ui/screen/QuizSummaryScreen.kt
+++ app/src/main/java/com/example/codingquiz/ui/screen/QuizSummaryScreen.kt
@@ -2,25 +2,22 @@ package com.example.codingquiz.ui.screen
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.IntrinsicSize
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -98,43 +95,38 @@ private fun QuizResultsList(results: List<QuizResult>) {
 
 @Composable
 private fun QuizResult(quizResult: QuizResult) {
-    val isAnswerCorrectInfo = stringResource(
-        id = if (quizResult.isAnswerCorrect) R.string.quiz_results_correct
-        else R.string.quiz_results_wrong
-    )
+    val answerIconResources = if (quizResult.isAnswerCorrect) {
+        Pair(
+            painterResource(id = R.drawable.ic_result_answer_correct),
+            stringResource(id = R.string.quiz_results_correct),
+        )
+    } else {
+        Pair(
+            painterResource(id = R.drawable.ic_result_answer_wrong),
+            stringResource(id = R.string.quiz_results_wrong),
+        )
+    }
 
-    val rightTextColor =
-        if (quizResult.isAnswerCorrect) Color.Green
-        else Color.Red
-
-    Row(
-        modifier = Modifier.height(intrinsicSize = IntrinsicSize.Max),
-        horizontalArrangement = Arrangement.spacedBy(8.dp),
-    ) {
-        Card(
-            modifier = Modifier.weight(0.75f),
+    Card {
+        Row(
+            modifier = Modifier
+                .padding(8.dp)
+                .height(intrinsicSize = IntrinsicSize.Max),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
         ) {
             Text(
-                modifier = Modifier.padding(8.dp),
+                modifier = Modifier
+                    .weight(0.8f)
+                    .align(Alignment.CenterVertically),
                 text = quizResult.questionText,
             )
-        }
 
-        Card(
-            modifier = Modifier
-                .fillMaxHeight()
-                .weight(0.25f),
-            colors = CardDefaults.cardColors(containerColor = rightTextColor),
-        ) {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center,
-            ) {
-                Text(
-                    modifier = Modifier.padding(8.dp),
-                    text = isAnswerCorrectInfo,
-                )
-            }
+            Icon(
+                modifier = Modifier
+                    .align(Alignment.CenterVertically),
+                painter = answerIconResources.first,
+                contentDescription = answerIconResources.second,
+            )
         }
     }
 }
